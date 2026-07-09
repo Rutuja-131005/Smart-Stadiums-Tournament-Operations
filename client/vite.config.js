db66643557f47edf5367ab5234ce6889e6151e3a
Rutuja <rutujajadhav475@gmail.com>
2026-07-09 16:51:48 +0530

    test: inline isomorphic-dompurify in client vitest config to avoid CI require ESM errors

--- client/vite.config.js
+++ client/vite.config.js
@@ -14,5 +14,10 @@ export default defineConfig({
     environment: 'jsdom',
     globals: true,
     setupFiles: './src/test/setup.js',
+    server: {
+      deps: {
+        inline: ['isomorphic-dompurify'],
+      },
+    },
   },
 });
